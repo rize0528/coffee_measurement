60e9c55bf041592ac9dbcc2f36aae9980c35903a
miaoski <miaoski@gmail.com>
2020-11-14 00:25:52 +0800

    add > param.h

--- gy33-agtron/gen-param-h.py
+++ gy33-agtron/gen-param-h.py
@@ -6,7 +6,7 @@ import json
 import sys
 
 if len(sys.argv) < 2:
-    print('Usage: python3 gen-param-h.py model.json')
+    print('Usage: python3 gen-param-h.py model.json > param.h')
     sys.exit(1)
 
 model = json.load(open(sys.argv[1]))
@@ -23,6 +23,7 @@ if model['model_name'] == 'linear_regression':
     print('#define BIAS {}'.format(model['reg_intercept']))
     print('#endif	// GY33_PARAM_H')
 elif model['model_name'] == 'MLP':
+    max_dim = 0
     print('''
 #ifndef GY33_PARAM_H
 #define MLP 1
@@ -31,6 +32,7 @@ elif model['model_name'] == 'MLP':
     wx = model['reg_coef']
     for i in range(len(wx)):
         x, y = len(wx[i]), len(wx[i][0])
+        max_dim = max(max_dim, max(x, y))
         print('mtx_type X{}[{}][{}] = {{'.format(i, x, y))
         for j in range(x):
             print('{%s},' % repr(wx[i][j])[1:-1])
@@ -39,6 +41,7 @@ elif model['model_name'] == 'MLP':
     for i in range(len(wx)):
         x = len(wx[i])
         print('mtx_type W{}[{}] = {{{}}};'.format(i, x, repr(wx[i])[1:-1]))
+    print('#define MAX_DIM {}'.format(max_dim))
     print('#endif	// GY33_PARAM_H')
 else:
     print('Unsupported model: {}'.format(model['model_name']))
